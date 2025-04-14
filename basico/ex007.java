6a445c9ef2786cc5e92d5e488edc46232cfb4637
AgathaTech <agatiinha.ct@gmail.com>
2025-04-14 20:37:56 -0300

    exercicio 007 - Programa para ler 2 valores e mostrar se são multiplos ou não são multiplos

---
+++ basico/ex007.java
@@ -0,0 +1,20 @@
+package basico;
+
+import java.util.Scanner;
+
+public class ex007 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A % B == 0 || B % A == 0) {
+            System.out.println("Sao Multiplos");
+        }
+        else {
+            System.out.println("Nao sao Multiplos");
+        }
+        sc.close();
+    }
+}
